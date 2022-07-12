9d2471837f3dd1a11592ef6e1dce5ed325b1f43d
publisher4m <101164920+publisher4m@users.noreply.github.com>
2022-07-12 18:56:14 +0900

    220712_mark02

--- assets/js/commonUi.js
+++ assets/js/commonUi.js
@@ -212,5 +212,39 @@ $tabButton.on('click', function(){
 })
 
 
+function section01_Swiper() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: '3',
+        speed: 800,
+        spaceBetween: 43,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints : {
+            768 : { //브라우저가 768보다 클 때
+              
+            },
+          
+        },
+    });
+}
+section01_Swiper();
 
+fileupload();
+function fileupload (){
+    var fileTarget = $('.filebox .upload-hidden');
+
+    fileTarget.on('change', function(){  // 값이 변경되면
+      if(window.FileReader){  // modern browser
+        var filename = $(this)[0].files[0].name;
+      } 
+      else {  // old IE
+        var filename = $(this).val().split('/').pop().split('\\').pop();  // 파일명만 추출
+      }
+      
+      // 추출한 파일명 삽입
+      $(this).siblings('.upload-name').val(filename);
+    });
+}
 
